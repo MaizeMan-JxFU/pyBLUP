071de3744e21f95649cdd8976cf0745a365eeb55
Jingxian <827888724@qq.com>
2025-10-03 10:41:30 +0800

    Fixed some bugs

--- test/demo.py
+++ test/demo.py
@@ -4,7 +4,7 @@ np.random.seed(2025)
 def mlm_test() -> None:
     from pyBLUP import BLUP
     snp_num = 10000
-    sample_num = 8000
+    sample_num = 2000
     pve = 0.5
     sigmau = 1
     x = np.zeros(shape=(sample_num,snp_num)) # 0,1,2 of SNP
@@ -22,11 +22,11 @@ def mlm_test() -> None:
         model = BLUP(y,None,x,kinship=i)
         model.fit()
         print((time.time()-t)/60,'mins')
-        break
-        # y_hat = model.predict(None,x)
-        # _+=y.tolist()
-        # _hat+=y_hat.tolist()
-        # real_pred = np.concatenate([np.array(_),np.array(_hat)],axis=1)
-        # print(f'{i}({round(model.pve,3)})',np.corrcoef(real_pred,rowvar=False)[0,1])
+        # break
+        y_hat = model.predict(None,x)
+        _+=y.tolist()
+        _hat+=y_hat.tolist()
+        real_pred = np.concatenate([np.array(_),np.array(_hat)],axis=1)
+        print(f'{i}({round(model.pve,3)})',np.corrcoef(real_pred,rowvar=False)[0,1])
 
 mlm_test()
