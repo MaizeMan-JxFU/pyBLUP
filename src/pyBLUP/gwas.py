f441ba45aa4f1c73139cea1ce2bdcdeba69da35d
Jingxian <827888724@qq.com>
2025-10-27 18:48:07 +0800

    add cpu inspect module and fixed some bugs

--- src/pyBLUP/gwas.py
+++ src/pyBLUP/gwas.py
@@ -5,8 +5,10 @@ from joblib import Parallel, delayed # for parallel processing
 import gc # garbage collection
 import time
 from .QC import QC
+from .cpu_inspect import get_process_info
+
 class GWAS:
-    def __init__(self,y:np.ndarray=None,X:np.ndarray=None,kinship:np.ndarray=None):
+    def __init__(self,y:np.ndarray=None,X:np.ndarray=None,kinship:np.ndarray=None,log:bool=True):
         '''
         Fast Solve of Mixed Linear Model by Brent.
         
@@ -14,6 +16,7 @@ class GWAS:
         :param X: Designed matrix for fixed effect nxp\n
         :param kinship: Calculation method of kinship matrix nxn
         '''
+        self.log = log
         X = np.concatenate([np.ones((y.shape[0],1)),X],axis=1) if X is not None else np.ones((y.shape[0],1))
         # simplify G matrix
         self.D, self.S, self.Dh = np.linalg.svd(kinship + 1e-6 * np.eye(y.shape[0]))
@@ -31,42 +34,52 @@ class GWAS:
         pass
     def _NULLREML(self,lbd: float):
         '''Restricted Maximum Likelihood Estimation (REML) of NULL'''
-        n,p_cov = self.Xcov.shape
-        p = p_cov
-        V = self.S+lbd
-        V_inv = 1/V
-        X_cov_snp = self.Xcov
-        XTV_invX = V_inv*X_cov_snp.T @ X_cov_snp
-        XTV_invy = V_inv*X_cov_snp.T @ self.y
-        beta = np.linalg.solve(XTV_invX,XTV_invy)
-        r = self.y - X_cov_snp@beta
-        rTV_invr = V_inv * r.T@r
-        log_detV = np.sum(np.log(V))
-        sign, log_detXTV_invX = np.linalg.slogdet(XTV_invX)
-        total_log = (n-p)*np.log(rTV_invr) + log_detV + log_detXTV_invX
-        c = (n-p)*(np.log(n-p)-1-np.log(2*np.pi))/2 # Contant
-        return c - total_log / 2
+        try:
+            n,p_cov = self.Xcov.shape
+            p = p_cov
+            V = self.S+lbd
+            V_inv = 1/V
+            X_cov_snp = self.Xcov
+            XTV_invX = V_inv*X_cov_snp.T @ X_cov_snp
+            XTV_invy = V_inv*X_cov_snp.T @ self.y
+            try:
+                beta = np.linalg.solve(XTV_invX, XTV_invy)
+            except np.linalg.LinAlgError:
+                beta = np.linalg.solve(XTV_invX+1e-6*np.eye(XTV_invX.shape[0]),XTV_invy)
+            r = self.y - X_cov_snp@beta
+            rTV_invr = V_inv * r.T@r
+            log_detV = np.sum(np.log(V))
+            sign, log_detXTV_invX = np.linalg.slogdet(XTV_invX)
+            total_log = (n-p)*np.log(rTV_invr) + log_detV + log_detXTV_invX
+            c = (n-p)*(np.log(n-p)-1-np.log(2*np.pi))/2 # Contant
+            return c - total_log / 2
+        except Exception as e:
+            print(f"REML 计算错误: {e}, lbd={lbd}")
+            return -1e12
     def _REML(self,lbd: float, snp_vec:np.array):
         '''Restricted Maximum Likelihood Estimation (REML)'''
-        n,p_cov = self.Xcov.shape
-        p = p_cov + 1
-        V = self.S+lbd
-        V_inv = 1/V
-        X_cov_snp = np.column_stack([self.Xcov, snp_vec])
-        XTV_invX = V_inv*X_cov_snp.T @ X_cov_snp
-        XTV_invy = V_inv*X_cov_snp.T @ self.y
         try:
-            beta = np.linalg.solve(XTV_invX, XTV_invy)
-        except np.linalg.LinAlgError:
-            # 添加正则项, 并尝试伪逆
-            beta = np.linalg.solve(XTV_invX+1e-6*np.eye(XTV_invX.shape[0]),XTV_invy)
-        r = self.y - X_cov_snp@beta
-        rTV_invr = V_inv * r.T@r
-        log_detV = np.sum(np.log(V))
-        sign, log_detXTV_invX = np.linalg.slogdet(XTV_invX)
-        total_log = (n-p)*np.log(rTV_invr) + log_detV + log_detXTV_invX
-        c = (n-p)*(np.log(n-p)-1-np.log(2*np.pi))/2 # Contant
-        return c - total_log / 2
+            n,p_cov = self.Xcov.shape
+            p = p_cov + 1
+            V = self.S+lbd
+            V_inv = 1/V
+            X_cov_snp = np.column_stack([self.Xcov, snp_vec])
+            XTV_invX = V_inv*X_cov_snp.T @ X_cov_snp
+            XTV_invy = V_inv*X_cov_snp.T @ self.y
+            try:
+                beta = np.linalg.solve(XTV_invX, XTV_invy)
+            except np.linalg.LinAlgError:
+                beta = np.linalg.solve(XTV_invX+1e-6*np.eye(XTV_invX.shape[0]),XTV_invy)
+            r = self.y - X_cov_snp@beta
+            rTV_invr = V_inv * r.T@r
+            log_detV = np.sum(np.log(V))
+            sign, log_detXTV_invX = np.linalg.slogdet(XTV_invX)
+            total_log = (n-p)*np.log(rTV_invr) + log_detV + log_detXTV_invX
+            c = (n-p)*(np.log(n-p)-1-np.log(2*np.pi))/2 # Contant
+            return c - total_log / 2
+        except Exception as e:
+            print(f"REML 计算错误: {e}, lbd={lbd}")
+            return -1e12
     def _fit(self,snp:np.ndarray=None):
         X = np.column_stack([self.Xcov, snp])
         n,p = X.shape
@@ -116,9 +129,16 @@ class GWAS:
             snp_chunk = self.Dh@snp_chunk
             def process_col(i):
                 return self._fit(snp_chunk[:, i])
-            results = np.array(Parallel(n_jobs=-1)(delayed(process_col)(i) for i in range(snp_chunk.shape[1])))
-            beta_se_p.append(np.concatenate([results,2*norm.sf(np.abs(results[:,0]/results[:,1])).reshape(-1,1)],axis=1))
-            print(f'''\r{round(100*chunk_indexs[ii+1]/num_snp,2)}% (time cost: {round((time.time()-t_start)/60,2)} mins, memory usage: {round((snp.nbytes+snp_chunk.nbytes)/1024**3,2)} G)''',end='')
+            if snp_chunk.shape[1]>0:
+                results = np.array(Parallel(n_jobs=-1)(delayed(process_col)(i) for i in range(snp_chunk.shape[1])))
+                beta_se_p.append(np.concatenate([results,2*norm.sf(np.abs(results[:,0]/results[:,1])).reshape(-1,1)],axis=1))
+            if self.log:
+                iter_ratio = chunk_indexs[ii+1]/num_snp
+                time_cost = time.time()-t_start
+                time_left = time_cost/iter_ratio
+                all_time_info = f'''{round(100*iter_ratio,2)}% (time cost: {round(time_cost/60,2)}/{round(time_left/60,2)} mins)'''
+                cpu,mem = get_process_info()
+                print(f'''\rCPU: {cpu}%, Memory: {round(mem,2)} G, Process: {all_time_info}''',end='')
             del snp_chunk,results # 释放内存
         print()
         self.snp_retain = snp_retain
@@ -155,7 +175,13 @@ class GWAS:
                 results = np.array(Parallel(n_jobs=-1)(delayed(process_col)(i) for i in range(snp_chunk.shape[1])))
                 beta_se_p.append(np.concatenate([results[:,[0,1]],2*norm.sf(np.abs(results[:,0]/results[:,1])).reshape(-1,1)],axis=1))
                 lbds.extend(results[:,2])
-            print(f'''\r{round(100*chunk_indexs[ii+1]/num_snp,2)}% (time cost: {round((time.time()-t_start)/60,2)} mins, memory usage: {round((snp.nbytes+snp_chunk.nbytes)/1024**3,2)} G)''',end='')
+            if self.log:
+                iter_ratio = chunk_indexs[ii+1]/num_snp
+                time_cost = time.time()-t_start
+                time_left = time_cost/iter_ratio
+                all_time_info = f'''{round(100*iter_ratio,2)}% (time cost: {round(time_cost/60,2)}/{round(time_left/60,2)} mins)'''
+                cpu,mem = get_process_info()
+                print(f'''\rCPU: {cpu}%, Memory: {round(mem,2)} G, Process: {all_time_info}''',end='')
             del snp_chunk,results # 释放内存
             gc.collect()
         print()
